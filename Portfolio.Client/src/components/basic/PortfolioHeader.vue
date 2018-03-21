285540b103cd519edeb8b3e5395aab0f7ff60b0d
KarolisKaj <karolis.kajenas1@gmail.com>
2018-03-21 23:31:34 +0200

    added icons with links, fix for colors required

--- Portfolio.Client/src/components/basic/PortfolioHeader.vue
+++ Portfolio.Client/src/components/basic/PortfolioHeader.vue
@@ -1,7 +1,14 @@
 <template>
-    <v-toolbar>
+    <v-toolbar >
       <v-toolbar-side-icon></v-toolbar-side-icon>
       <v-toolbar-title>Portfolio</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon href='https://stackoverflow.com/users/5505949'>
+        <v-icon>fab fa-stack-overflow</v-icon>
+      </v-btn>
+      <v-btn icon href='https://github.com/KarolisKaj'>
+        <v-icon>fab fa-github-square</v-icon>
+      </v-btn>
     </v-toolbar>
 </template>
 
