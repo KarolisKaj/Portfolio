da81fbb058145bebc7d3a06f71fc1d8cc2d8786c
KarolisKaj <karolis.kajenas1@gmail.com>
2018-04-12 22:35:38 +0300

    add unindexed files

---
+++ Portfolio/Portfolio.DataStore/Model.fs
@@ -0,0 +1,10 @@
+﻿module Model
+    type Article = {
+        id : string
+        articleBodyId :string
+        name :string
+        title : string
+        teaser : string 
+        images : string array
+        keywords : string array
+        }
---
+++ Portfolio/Portfolio/Controllers/ArticleController.fs
@@ -0,0 +1,28 @@
+﻿namespace Portfolio.Controllers
+
+open Microsoft.AspNetCore.Mvc
+open Portfolio.DataStore.DataStore
+
+[<Route("api/v1/[controller]")>]
+type ArticleController () =
+    inherit Controller()
+
+    [<HttpGet>]
+    member this.Get() =
+        ObtainArticles
+
+    [<HttpGet("{id}")>]
+    member this.Get(id:string) =
+        ObtainArticle id 
+
+    [<HttpPost>]
+    member this.Post([<FromBody>]value:string) =
+        ()
+
+    [<HttpPut("{id}")>]
+    member this.Put(id:int, [<FromBody>]value:string ) =
+        ()
+
+    [<HttpDelete("{id}")>]
+    member this.Delete(id:int) =
+        ()
