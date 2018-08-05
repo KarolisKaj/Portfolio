7c32d178574d30ea730c500e8087af2102254385
KarolisKaj <karolis.kajenas1@gmail.com>
2018-08-05 20:10:01 +0300

    moved to fb functions with sendgrid

---
+++ Portfolio/src/index.js
@@ -0,0 +1,16 @@
+const functions = require('firebase-functions');
+
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+const SENDGRID_API_KEY = functions.config().sendgrid.key;
+
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(SENDGRID_API_KEY);
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
