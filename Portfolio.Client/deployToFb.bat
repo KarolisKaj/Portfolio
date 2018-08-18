b8048936c8618a8a3c96319a8cd334fffbb4565c
KarolisKaj <karolis.kajenas1@gmail.com>
2018-08-18 17:38:26 +0300

    Recovered correct deployment script.

--- Portfolio.Client/deployToFb.bat
+++ Portfolio.Client/deployToFb.bat
@@ -4,5 +4,5 @@ xcopy /s .\dist c:\tempDeploy\public\ /Y
 rmdir /s /q .\dist
 cd c:\tempDeploy 
 call firebase login
-call firebase -P portfolio-66a1b init functions
+call firebase -P portfolio-66a1b init hosting
 call firebase -P portfolio-66a1b deploy
